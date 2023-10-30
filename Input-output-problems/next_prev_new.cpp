fc51796be8a9262d8bb0162f4b6a6ec776b6fac8
zlatatikhomirova <72922674+zlatatikhomirova@users.noreply.github.com>
2023-10-30 17:44:36 +0700

    Create next_prev_new.cpp

---
+++ Input-output-problems/next_prev_new.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	short a;
+	cin >> a;
+	cout << "The next number for the number " << a << " is " << a + 1 << ".\n";
+	cout << "The previous number for the number " << a << " is " << a - 1 << ".";
+	return 0;
+}
