a24023d45b779a8bb9c4059289566431a0f7413c
fatfoot15 <104977112+fatfoot15@users.noreply.github.com>
2023-12-04 17:33:34 +0700

    Create T_simmetrichnoe_chislo.cpp

---
+++ Input-output-problems/T_simmetrichnoe_chislo.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main() {
+	int n, n1, n2, n3, n4, ns1, ns2;
+	cin >> n;
+	n1 = n / 1000;
+	n2 = n / 100 % 10;
+	n3 = n / 10 % 10;
+	n4 = n % 10;
+	ns1 = n1 - n4;
+	ns2 = n2 - n3;
+	cout << 1 + ns1 * 9 + ns2;
+}
