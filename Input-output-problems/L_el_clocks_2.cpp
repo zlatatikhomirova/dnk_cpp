5502d473d265ae018a017ac3dc9e2305849c90b7
fatfoot15 <104977112+fatfoot15@users.noreply.github.com>
2023-12-04 17:19:24 +0700

    Create L_el_clocks_2.cpp

---
+++ Input-output-problems/L_el_clocks_2.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    cout << a / 3600 % 24 << ":" << a % 3600 / 60 / 10 << a % 3600 / 60 % 10 << ":" << a % 60 / 10 << a % 60 % 10;
+}
