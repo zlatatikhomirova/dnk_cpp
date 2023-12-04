472c2850e80be8963e37df033231ae40f3bed238
fatfoot15 <104977112+fatfoot15@users.noreply.github.com>
2023-12-04 18:12:01 +0700

    Create F_num_max_element.cpp

---
+++ Search/linear_search/F_num_max_element.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <malloc.h>
+
+using namespace std;
+
+int main()
+{
+    unsigned short n;
+    int maxi, num;
+    maxi = -1001;
+    cin >> n;
+    int* a;
+    a = (int*)malloc(sizeof(int) * n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if (a[i] > maxi) {
+            maxi = a[i];
+            num = i + 1;
+        }
+    }
+    cout << num;
+}
