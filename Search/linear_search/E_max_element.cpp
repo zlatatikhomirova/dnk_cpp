f0652ca4d02f9782c76d0009bcb1e637472914b4
fatfoot15 <104977112+fatfoot15@users.noreply.github.com>
2023-12-04 18:10:07 +0700

    Create E_max_element.cpp

---
+++ Search/linear_search/E_max_element.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <malloc.h>
+
+using namespace std;
+
+int main()
+{
+    short n, t, maxi;
+    maxi = -1001;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> t;
+        if (t > maxi) maxi = t;
+    }
+    cout << maxi;
+}
