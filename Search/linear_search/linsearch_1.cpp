2a1dfa8ddecc8164ef961a3f71e1db68ffb8ae14
zlatatikhomirova <72922674+zlatatikhomirova@users.noreply.github.com>
2023-12-04 12:41:46 +0200

    Create linsearch_1.cpp

---
+++ Search/linear_search/linsearch_1.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+    
+    unsigned short N, cnt=0;
+    short x;
+    cin >> N;
+    vector<int>v(N);
+    for (int i = 0; i < N; i++)
+        cin >> v[i];
+    cin >> x;
+    for (int i = 0; i < N;)
+        if (v[i++] == x)
+            cnt++;
+    cout << cnt;
+    return 0;
+}
