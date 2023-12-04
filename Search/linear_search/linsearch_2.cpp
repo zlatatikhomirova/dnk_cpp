0cdc1bd73c9e95d2dc520e87693053e411185352
zlatatikhomirova <72922674+zlatatikhomirova@users.noreply.github.com>
2023-12-04 12:42:25 +0200

    Create linsearch_2.cpp

---
+++ Search/linear_search/linsearch_2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include<malloc.h>
+using namespace std;
+
+int main()
+{
+    
+    unsigned short N, cnt=0;
+    short x;
+    cin >> N;
+    int *A;
+    A = (int*)malloc(sizeof(int) * N);
+    for (int i = 0; i < N; i++)
+        cin >> A[i];
+    cin >> x;
+    for (int i = 0; i < N;)
+        if (A[i++] == x) {
+            cout << "YES" << endl;
+            return 0;
+        }
+    cout << "NO";
+    return 0;
+}
