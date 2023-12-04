977da0aa6c2865d63bead3593c0bf87687dead7d
zlatatikhomirova <72922674+zlatatikhomirova@users.noreply.github.com>
2023-12-04 12:47:26 +0200

    Create C_nearest_number.cpp

---
+++ Search/linear_search/C_nearest_number.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include<malloc.h>
+using namespace std;
+
+int main()
+{
+    
+    unsigned short N;
+    short x, minn, y;
+    cin >> N;
+    int *A;
+    A = (int*)malloc(sizeof(int) * N);
+    for (int i = 0; i < N; i++)
+        cin >> A[i];
+    cin >> x;
+    minn = 2001;
+    for (int i = 0; i < N; i++) {
+        int tmp = abs(x - A[i]);
+        if (abs(x - A[i]) < minn) {
+            y = A[i];
+            minn = tmp;
+        }
+    }
+    cout << y;
+    return 0;
+}
